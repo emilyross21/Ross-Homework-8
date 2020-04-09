d7c6443f61731f6cceae34aa75a9712cfdcc0ba2
Emily Ross <e.ross2@setonhill.edu>
2020-04-09 19:54:38 -0400

    Initial Commit

---
+++ Ross Homework 8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ross Homework 8
+//
+//  Created by Emily Ross on 4/9/20.
+//  Copyright © 2020 Emily Ross. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
